2b447edc940d85a544e576f3b8514ba47c231200
Tom Zhang <tommyawesome08@gmail.com>
2025-08-04 22:02:42 -0400

    Tom's Notes (TNotes) initial commit

---
+++ tnotes-api/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('TNotes API is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
